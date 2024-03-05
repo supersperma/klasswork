7bc34805602dadff6861dc25cdff3087461ca46c
supersperma <158447987+supersperma@users.noreply.github.com>
2024-03-05 09:43:26 +0300

    Create 05mart.py

---
+++ 05mart.py
@@ -0,0 +1,30 @@
+# Задание 1
+P = int(input())
+a = 1
+while a < P:
+    print(a)
+    a += 1
+#Задание 2
+A = int(input())
+B = int(input())
+z = A + 1
+while z < B:
+    print(z)
+    z += 1
+#Задание 3
+A = int(input())
+B = int(input())
+v = B - 1
+while v > A:
+    print(v)
+    v -= 1
+#Задание 4
+N = int(input())
+K = N // 3 + 1  
+print("Наименьшее целое K:", K)
+print("Значение 3K:", 3 * K)
+#Задание 5
+N = 10  
+K = (N - 1) // 3  
+print("Наибольшее целое K:", K)
+print("Значение 3K:", 3 * K)
